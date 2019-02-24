c65208dc38fd7177fe238d6792b16ca90abaa88b
sveitser <sveitser@gmail.com>
2019-02-24 22:55:01 +0800

    Merge with vortex pok3r layout where possible

--- keyboards/minidox/keymaps/sveitser/keymap.c
+++ keyboards/minidox/keymaps/sveitser/keymap.c
@@ -11,6 +11,25 @@ extern keymap_config_t keymap_config;
 #define _RAISE 2
 #define _ADJUST 16
 
+#define _FN_R 3
+
+#define my_a MT(MOD_LCTL, KC_A)
+#define my_s MT(MOD_LSFT, KC_S)
+#define my_d MT(MOD_LGUI, KC_D)
+#define my_f LT(   _FN_R, KC_F)
+/* #define my_g LT(    _NUM, KC_G) */
+/* #define my_g LT(    _RAISE, KC_G) */
+#define my_ent MT(MOD_LALT, KC_E)
+
+/* #define my_i MT(MOD_LALT, KC_I) */
+/* #define my_spc LT( _SYMB, KC_SPC) */
+#define my_spc MT(MOD_LALT, KC_SPC)
+/* #define my_j LT(   _FN_L, KC_J) */
+#define my_j LT(  _LOWER, KC_J)
+#define my_k MT(MOD_RGUI, KC_K)
+#define my_l MT(MOD_RSFT, KC_L)
+#define my_scln MT(MOD_RCTL, KC_SCLN)
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -24,93 +43,40 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  | Shift| LOWER|      |    |      | RAISE| Ctrl |
- *                  `-------------|BkSpac|    | Space|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_QWERTY] = LAYOUT( \
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  my_a,    my_s,    my_d,    my_f,    KC_G,         KC_H,    my_j,    my_k,    my_l,    my_scln, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    OSM(MOD_LSFT), LOWER, KC_BSPC,         KC_SPC, RAISE, KC_LCTL                 \
+                    _______, LOWER, my_ent,         my_spc, RAISE, _______                 \
 ),
 
-/* Raise
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab | Left | Down |  Up  | Right|           |      |   -  |   =  |   [  |   ]  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt |      |           |      |      |      |   \  |   '  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_RAISE] = LAYOUT( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT, \
+  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
+  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, \
                     _______, _______, _______,      _______, _______, _______                    \
 ),
 
-/* Lower
- *
- * ,----------------------------------.           ,----------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   ~  |      |      |      |           |      |      |      |   |  |   "  |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|  Del |
- *                  `-------------|      |    | Enter|------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_LOWER] = LAYOUT( \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
-  KC_ESC,  _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
+  KC_BSPC,  KC_ESC,  KC_TAB, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
   KC_CAPS, KC_TILD, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, \
-                    _______, _______, _______,      KC_ENT,  _______, KC_DEL                    \
+                    _______, _______, _______,      KC_ENT,  _______, KC_DEL                     \
 ),
 
-/* Adjust (Lower + Raise)
- *
- * ,----------------------------------.           ,----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  Up  |  F9  |  F10 |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |RGBSAI|RGBSAD|           |      | Left | Down |Right |caltde|
- * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|RGBTOG|RGBMOD|RGBHUI|RGBHUD|           |RGBVAI|RGBVAD|  F8  |Taskmg|      |
- * `----------------------------------'           `----------------------------------'
- *                  ,--------------------.    ,------,-------------.
- *                  |      | LOWER|      |    |      | RAISE|      |
- *                  `-------------|      |    |      |------+------.
- *                                |      |    |      |
- *                                `------'    `------'
- */
 [_ADJUST] =  LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_UP,   KC_F9,   KC_F10, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_UP,   KC_F9,  KC_F10, \
   KC_F11,  KC_F12,  _______, RGB_SAI, RGB_SAD,      _______, KC_LEFT, KC_DOWN, KC_RGHT, CALTDEL, \
-  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD, KC_F8, TSKMGR,  _______, \
-                    _______, _______, _______,      _______,  _______, _______                    \
-)
+  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD,      RGB_VAI, RGB_VAD,   KC_F8, TSKMGR,  _______, \
+                    _______, _______, _______,      _______, _______, _______                    \
+),
+
+[_FN_R] = LAYOUT(\
+_______, _______, _______, _______, _______,       KC_DOWN, KC_RIGHT,  _______, _______, _______, \
+_______, _______, _______, _______, _______,       KC_LEFT, KC_HOME,  KC_PGUP,  KC_PGDN, KC_END,  \
+_______, _______, _______, _______, _______,       KC_UP, _______, _______, _______, _______,     \
+                  _______, _______, _______,       _______, _______, _______                      \
+             )
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {

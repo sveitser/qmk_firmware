6a18642ca3910cf5fa9707fe23628afb55c0bad1
sveitser <sveitser@gmail.com>
2019-07-10 22:16:46 +0800

    Swap tab/backspace, tab now main left thumb key

--- keyboards/minidox/keymaps/sveitser/keymap.c
+++ keyboards/minidox/keymaps/sveitser/keymap.c
@@ -28,7 +28,7 @@
 
 // thumb left
 #define raise_spc LT(_RAISE, KC_SPC)
-#define low_bspc LT(_LOWER, KC_BSPC)
+#define low_tab LT(_LOWER, KC_TAB)
 
 // thumb right
 #define raise_esc LT(_RAISE, KC_ESC)
@@ -45,19 +45,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    lalt,    ladj,    KC_T,         KC_Y,    radj,    ralt,    KC_O,    KC_P,    \
   fnr,    lsft,    lgui,     lctl,    KC_G,         KC_H,    rctl,    rgui,    rsft,    KC_SCLN,    \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-            KC_TAB,  raise_esc,   low_bspc,     raise_spc,  raise_ent,   KC_DEL                  \
+            KC_BSPC,  raise_esc,   low_tab,     raise_spc,  raise_ent,   KC_DEL                  \
 ),
 
 [_RAISE] = LAYOUT( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  _______,  _______, _______, _______, _______,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
+  KC_BSPC,  KC_ESC,  KC_TAB, _______, _______,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
   _______, _______, _______, _______, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, \
                     _______, _______, _______,      _______, _______, _______                    \
 ),
 
 [_LOWER] = LAYOUT( \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
-  KC_BSPC,  KC_ESC,  KC_TAB, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
+  _______,  _______, _______, _______, _______,     _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
   KC_CAPS, KC_TILD, _______, _______, _______,      _______, _______, _______, KC_PIPE,  KC_DQT, \
                     _______, _______, _______,      KC_ENT,  _______, KC_DEL                     \
 ),

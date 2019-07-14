2de7f5b329aa153029ec54b4036141df0b006f9a
sveitser <sveitser@gmail.com>
2019-07-14 14:15:52 +0800

    Add colemak layer

--- keyboards/vortex/keymaps/sveitser/keymap.c
+++ keyboards/vortex/keymaps/sveitser/keymap.c
@@ -17,25 +17,49 @@
 
 #include "vortex.h"
 
-#define _FN_R 1
-#define _FN_L 2
-#define _SYMB 3
-#define _NUM 4
+#define _QWERTY 0
+#define _COLEMAK 1
+#define _FN_R 2
+#define _FN_L 3
+#define _SYMB 4
+#define _NUM 5
 
-#define my_f MT(MOD_LCTL, KC_F)
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  COLEMAK,
+  FN_R,
+  FN_L,
+  SYMB,
+  NUM,
+};
+
+#define my_a LT(   _FN_R, KC_A)
 #define my_s MT(MOD_LSFT, KC_S)
 #define my_d MT(MOD_LGUI, KC_D)
-#define my_a LT(   _FN_R, KC_A)
+#define my_f MT(MOD_LCTL, KC_F)
 #define my_g LT(    _NUM, KC_G)
 #define my_e MT(MOD_LALT, KC_E)
 
 #define my_i MT(MOD_LALT, KC_I)
 #define my_spc LT( _SYMB, KC_SPC)
 #define my_scln LT(   _FN_L, KC_SCLN)
-#define my_k MT(MOD_RGUI, KC_K)
 #define my_l MT(MOD_RSFT, KC_L)
+#define my_k MT(MOD_RGUI, KC_K)
 #define my_j MT(MOD_RCTL, KC_J)
 
+#define cm_a LT(   _FN_R, KC_A)
+#define cm_r MT(MOD_LSFT, KC_R)
+#define cm_s MT(MOD_LGUI, KC_S)
+#define cm_t MT(MOD_LCTL, KC_T)
+#define cm_d LT(    _NUM, KC_D)
+#define cm_f MT(MOD_LALT, KC_F)
+
+#define cm_u MT(MOD_LALT, KC_U)
+#define cm_spc LT( _SYMB, KC_SPC)
+#define cm_o LT(   _FN_L, KC_O)
+#define cm_i MT(MOD_RSFT, KC_I)
+#define cm_e MT(MOD_RGUI, KC_E)
+#define cm_n MT(MOD_RCTL, KC_N)
         /* _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,      _______, */
         /* _______,     _______, _______, _______, _______, _______,          _______, _______,  _______, _______, _______, _______, _______, _______, */
         /* _______,         _______, _______, _______, _______, _______,          _______, _______,  _______, _______, _______,  _______,    _______, */
@@ -44,11 +68,18 @@
 
 const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(KEYMAP_60_ANSI)
-    [0] = LAYOUT_60_ansi(
+    [_QWERTY] = LAYOUT_60_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    my_e,    KC_R,    KC_T,    KC_Y,    KC_U,    my_i,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS, my_a,    my_s,    my_d,    my_f,    my_g,    KC_H,    my_j,    my_k,    my_l, my_scln,    KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, my_spc,  KC_RALT, MO(1),   MO(2), KC_RCTL
+    ),
+    [_COLEMAK] = LAYOUT_60_ansi(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    cm_f,    KC_P,    KC_G,    KC_J,    KC_L,    cm_u,    KC_Y,    KC_SCLN,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_BSPC, cm_a,    cm_r,    cm_s,    cm_t,    cm_d,    KC_H,    cm_n,    cm_e,    cm_i,    cm_o,    KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, my_spc,  KC_RALT, MO(1),   MO(2), KC_RCTL
     ),
     [_FN_R] = LAYOUT_60_ansi(
@@ -60,9 +91,9 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_FN_L] = LAYOUT_60_ansi(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______,     _______, _______, _______, _______, _______,          _______, _______,  _______, _______, _______, _______, _______, _______,
+        _______,     QWERTY, COLEMAK, _______, _______, _______,          _______, _______,  _______, _______, _______, _______, _______, _______,
         _______,          KC_BSPC,  KC_ESC,  KC_TAB,  KC_ENT, _______,          _______, _______,  _______, _______, _______,  _______,    _______,
-        _______,            _______, _______,  _______, _______, _______,           _______, _______, _______, _______, _______,           _______,
+        _______,        _______, _______,  _______, _______, _______,           _______, _______, _______, _______, _______,           _______,
         _______,     _______,    _______,                                 _______                        ,    _______,  _______, _______, _______
     ),
     [_SYMB] = LAYOUT_60_ansi(
@@ -123,10 +154,28 @@ void matrix_scan_user(void) {
 
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
-}
-
 void led_set_user(uint8_t usb_led) {
 
 }
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+        case QWERTY:
+          if (record->event.pressed) {
+            persistent_default_layer_set(1UL<<_QWERTY);
+          }
+          return false;
+          break;
+        case COLEMAK:
+          if (record->event.pressed) {
+            persistent_default_layer_set(1UL<<_COLEMAK);
+          }
+          return false;
+          break;
+  }
+    return true;
+};
